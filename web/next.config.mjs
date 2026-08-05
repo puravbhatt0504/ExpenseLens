90770c2ff22e31a2bca8618c0ca4d42817c497f5
puravbhatt0504 <pbhatt340@gmail.com>
2026-08-05 19:45:31 +0530

    fix: oauth cors headers

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
